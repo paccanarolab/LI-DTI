599c76d693efc4a816b1891cb93e4b70c52e0886
Santiago Ferreyra <108885016+santiferreyra@users.noreply.github.com>
2026-01-19 16:18:57 -0300

    Delete code directory

--- code/get_predictions.m
+++
@@ -1,9 +0,0 @@
-function [Res] = get_predictions( params, R, SIR, SIC)
-    
-    gamma = params(1);
-    bethas = params(2:1+length(SIR));
-    alphas = params(2+length(SIR):length(params));
-
-    Res = our_method(R, gamma, SIR, SIC , alphas, bethas);
-
-end
